8879ec6983ca142fa2939dea1143e2c45d6d037e
Arttu Kuikka <75498768+ArttuKuikka@users.noreply.github.com>
2023-06-04 23:11:29 +0300

    debug

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ $(document).ready(function(){
 		e.preventDefault();
 		return "Oletko varma että haluat poistua";
 	});
+
+	var ifra = $('iframe').contents().find('.answer');
+	ifra[0].addEventListener('input', function(){
+		let k = ifra.html();
+		console.log( "proo test: " + btoa(k.toString()));
+	})
+	
 });
