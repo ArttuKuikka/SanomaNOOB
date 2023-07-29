825485cdca5ade9413298b907e0d68ebee1ea3ce
Arttu Kuikka <arttu.juhani.kuikka@gmail.com>
2023-07-29 13:12:44 +0300

    ensimmäinen julkaistava versio 0.0.1

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 
-	$(window).on('beforeunload', function(e) {
-		return 'Oletko varma että haluat poistua?';
+	// $(window).on('beforeunload', function(e) {
+	// 	e.preventDefault();
+	// 	return "Oletko varma että haluat poistua?";
+	// });
+
+	window.addEventListener('beforeunload',(event) =>{
+		event.preventDefault();
 	});
 });
